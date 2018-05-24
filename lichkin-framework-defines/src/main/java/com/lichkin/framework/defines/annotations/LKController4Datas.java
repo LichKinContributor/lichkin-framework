73d1fca65b2a0eb2a641733e0859ef80c7573f36
china-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-05-25 04:33:29 +0800

    javadoc完善

--- lichkin-framework-defines/src/main/java/com/lichkin/framework/defines/annotations/LKController4Datas.java
+++ lichkin-framework-defines/src/main/java/com/lichkin/framework/defines/annotations/LKController4Datas.java
@@ -1,8 +1,6 @@
 package com.lichkin.framework.defines.annotations;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -14,8 +12,6 @@ import java.lang.annotation.Target;
  *
  * @author SuZhou LichKin Information Technology Co., Ltd.
  */
-@Documented
-@Inherited
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LKController4Datas {
--- lichkin-framework-defines/src/main/java/com/lichkin/framework/defines/annotations/LKWithoutLogin.java
+++ lichkin-framework-defines/src/main/java/com/lichkin/framework/defines/annotations/LKWithoutLogin.java
@@ -1,8 +1,6 @@
 package com.lichkin.framework.defines.annotations;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -15,8 +13,6 @@ import java.lang.annotation.Target;
  *
  * @author SuZhou LichKin Information Technology Co., Ltd.
  */
-@Documented
-@Inherited
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LKWithoutLogin {
