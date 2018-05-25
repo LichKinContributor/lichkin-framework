4beb627d6bfe9aaae750c05d3712276606f15efa
china-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-05-25 09:25:56 +0800

    接口定义完善

--- lichkin-framework-db/src/main/java/com/lichkin/framework/db/entities/LKUserInterface.java
+++ lichkin-framework-db/src/main/java/com/lichkin/framework/db/entities/LKUserInterface.java
@@ -1,28 +1,12 @@
 package com.lichkin.framework.db.entities;
 
-import com.lichkin.framework.db.entities.suppers.LKBaseSysInterface;
 import com.lichkin.framework.defines.enums.impl.LKAuthenticationEnum;
-import com.lichkin.framework.defines.enums.impl.LKGenderEnum;
 
 /**
  * 用户接口
  * @author SuZhou LichKin Information Technology Co., Ltd.
  */
-public interface LKUserInterface extends LKBaseSysInterface {
-
-	/**
-	 * 获取姓名
-	 * @return 姓名
-	 */
-	public String getUserName();
-
-
-	/**
-	 * 设置姓名
-	 * @param userName 姓名
-	 */
-	public void setUserName(String userName);
-
+public interface LKUserInterface extends LKBaseUserInterface {
 
 	/**
 	 * 获取身份证号
@@ -36,20 +20,6 @@ public interface LKUserInterface extends LKBaseSysInterface {
 	 * @param userCard 身份证号
 	 */
 	public void setUserCard(final String userCard);
-
-
-	/**
-	 * 获取性别
-	 * @return 性别
-	 */
-	public LKGenderEnum getGender();
-
-
-	/**
-	 * 设置性别
-	 * @param gender 性别
-	 */
-	public void setGender(final LKGenderEnum gender);
 
 
 	/**
@@ -137,184 +107,16 @@ public interface LKUserInterface extends LKBaseSysInterface {
 
 
 	/**
-	 * 获取头像
-	 * @return 头像
+	 * 获取实名认证等级
+	 * @return 实名认证等级
 	 */
-	public String getPhoto();
-
-
-	/**
-	 * 设置头像
-	 * @param photo 头像
-	 */
-	public void setPhoto(final String photo);
-
-
-	/**
-	 * 获取手机号码
-	 * @return 手机号码
-	 */
-	public String getCellphone();
-
-
-	/**
-	 * 设置手机号码
-	 * @param cellphone 手机号码
-	 */
-	public void setCellphone(final String cellphone);
-
-
-	/**
-	 * 获取电话号码
-	 * @return 电话号码
-	 */
-	public String getTelephone1();
-
-
-	/**
-	 * 设置电话号码
-	 * @param telephone1 电话号码
-	 */
-	public void setTelephone1(String telephone1);
-
-
-	/**
-	 * 获取电话号码
-	 * @return 电话号码
-	 */
-	public String getTelephone2();
-
-
-	/**
-	 * 设置电话号码
-	 * @param telephone2 电话号码
-	 */
-	public void setTelephone2(String telephone2);
-
-
-	/**
-	 * 获取电话号码
-	 * @return 电话号码
-	 */
-	public String getTelephone3();
-
-
-	/**
-	 * 设置电话号码
-	 * @param telephone3 电话号码
-	 */
-	public void setTelephone3(String telephone3);
-
-
-	/**
-	 * 获取邮箱
-	 * @return 邮箱
-	 */
-	public String getEmail();
-
-
-	/**
-	 * 设置邮箱
-	 * @param email 邮箱
-	 */
-	public void setEmail(final String email);
-
-
-	/**
-	 * 获取邮箱
-	 * @return 邮箱
-	 */
-	public String getEmail1();
-
-
-	/**
-	 * 设置邮箱
-	 * @param email1 邮箱
-	 */
-	public void setEmail1(String email1);
-
-
-	/**
-	 * 获取邮箱
-	 * @return 邮箱
-	 */
-	public String getEmail2();
-
-
-	/**
-	 * 设置邮箱
-	 * @param email2 邮箱
-	 */
-	public void setEmail2(String email2);
-
-
-	/**
-	 * 获取邮箱
-	 * @return 邮箱
-	 */
-	public String getEmail3();
-
-
-	/**
-	 * 设置邮箱
-	 * @param email3 邮箱
-	 */
-	public void setEmail3(String email3);
-
-
-	@Override
-	public String getCheckCode();
-
-
-	@Override
-	public void setCheckCode(String checkCode);
-
-
 	public LKAuthenticationEnum getAuthentication();
 
 
+	/**
+	 * 设置实名认证等级
+	 * @param authentication 实名认证等级
+	 */
 	public void setAuthentication(LKAuthenticationEnum authentication);
-
-
-	/**
-	 * 获取备用字段1
-	 * @return 备用字段1
-	 */
-	public String getField1();
-
-
-	/**
-	 * 设置备用字段1
-	 * @param field1 备用字段1
-	 */
-	public void setField1(String field1);
-
-
-	/**
-	 * 获取备用字段2
-	 * @return 备用字段2
-	 */
-	public String getField2();
-
-
-	/**
-	 * 设置备用字段2
-	 * @param field2 备用字段2
-	 */
-	public void setField2(String field2);
-
-
-	/**
-	 * 获取备用字段3
-	 * @return 备用字段3
-	 */
-	public String getField3();
-
-
-	/**
-	 * 设置备用字段3
-	 * @param field3 备用字段3
-	 */
-	public void setField3(String field3);
 
 }
--- lichkin-framework-db/src/main/java/com/lichkin/framework/db/entities/LKUserLoginInterface.java
+++ lichkin-framework-db/src/main/java/com/lichkin/framework/db/entities/LKUserLoginInterface.java
@@ -6,7 +6,7 @@ import com.lichkin.framework.db.entities.suppers.LKBaseSysInterface;
  * 用户登录接口
  * @author SuZhou LichKin Information Technology Co., Ltd.
  */
-public interface LKUserLoginInterface extends LKBaseSysInterface {
+public interface LKUserLoginInterface extends LKBaseSysInterface, LKExtendFields {
 
 	/**
 	 * 获取用户ID
@@ -146,47 +146,5 @@ public interface LKUserLoginInterface extends LKBaseSysInterface {
 	 * @param wechatId 微信ID
 	 */
 	public void setWechatId(String wechatId);
-
-
-	/**
-	 * 获取备用字段1
-	 * @return 备用字段1
-	 */
-	public String getField1();
-
-
-	/**
-	 * 设置备用字段1
-	 * @param field1 备用字段1
-	 */
-	public void setField1(String field1);
-
-
-	/**
-	 * 获取备用字段2
-	 * @return 备用字段2
-	 */
-	public String getField2();
-
-
-	/**
-	 * 设置备用字段2
-	 * @param field2 备用字段2
-	 */
-	public void setField2(String field2);
-
-
-	/**
-	 * 获取备用字段3
-	 * @return 备用字段3
-	 */
-	public String getField3();
-
-
-	/**
-	 * 设置备用字段3
-	 * @param field3 备用字段3
-	 */
-	public void setField3(String field3);
 
 }
