7f5f61b308956c1e302bd9452409e02be01ce3d0
china-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-05-26 13:31:20 +0800

    Order继承__COLUMN

--- lichkin-framework-db/src/main/java/com/lichkin/framework/db/beans/Order.java
+++ lichkin-framework-db/src/main/java/com/lichkin/framework/db/beans/Order.java
@@ -6,16 +6,13 @@ import static com.lichkin.framework.defines.LKStringStatics.BLANK;
  * 排序信息对象
  * @author SuZhou LichKin Information Technology Co., Ltd.
  */
-public class Order extends __SQL {
+public class Order extends __COLUMN {
 
 	/** ASC */
 	private static final String ASC = "ASC";
 
 	/** DESC */
 	private static final String DESC = "DESC";
-
-	/** 列资源ID */
-	final int columnResId;
 
 	/** 是否正序 */
 	final boolean asc;
@@ -24,18 +21,26 @@ public class Order extends __SQL {
 	/**
 	 * 构造方法
 	 * @param columnResId 列资源ID
+	 */
+	public Order(int columnResId) {
+		this(columnResId, true);
+	}
+
+
+	/**
+	 * 构造方法
+	 * @param columnResId 列资源ID
 	 * @param asc true:正序;false倒序.
 	 */
 	public Order(int columnResId, boolean asc) {
-		super();
-		this.columnResId = columnResId;
+		super(columnResId);
 		this.asc = asc;
 	}
 
 
 	@Override
 	StringBuilder getSQL(boolean useSQL) {
-		return new StringBuilder().append(BLANK).append(getColumnSQL(useSQL, columnResId)).append(BLANK).append(asc ? ASC : DESC);
+		return new StringBuilder().append(BLANK).append(super.getSQL(useSQL)).append(BLANK).append(asc ? ASC : DESC);
 	}
 
 }
