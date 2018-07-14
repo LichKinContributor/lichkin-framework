56917138fde0cdc464d6ae5987c4d4a662525caf
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-07-14 17:33:55 +0800

    修改API定义

--- lichkin-framework-defines/src/main/java/com/lichkin/framework/defines/LKFrameworkStatics.java
+++ lichkin-framework-defines/src/main/java/com/lichkin/framework/defines/LKFrameworkStatics.java
@@ -32,8 +32,11 @@ public class LKFrameworkStatics {
 	/** 系统默认配置值：数据请求映射。 */
 	public static final String WEB_MAPPING_DATAS = ".do";
 
-	/** 系统默认配置值：API数据请求映射。 */
-	public static final String WEB_MAPPING_API = "/API";
+	/** 系统默认配置值：API数据请求映射（仅应用于客户端请求）。 */
+	public static final String WEB_MAPPING_API_APP = "/API/App";
+
+	/** 系统默认配置值：API数据请求映射（仅应用于页面请求）。 */
+	public static final String WEB_MAPPING_API_WEB = "/API/Web";
 
 	/** 系统默认配置值：页面请求映射。 */
 	public static final String WEB_MAPPING_PAGES = ".dhtml";
