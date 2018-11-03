a97da7f201d8deca1498c997f3e45a20ca9079e7
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-11-03 20:06:08 +0800

    错误编码范围调整，每一万一个段，通过段名取相应目录。

--- lichkin-framework-utils/src/main/java/com/lichkin/framework/utils/i18n/LKI18NReader4ErrorCodes.java
+++ lichkin-framework-utils/src/main/java/com/lichkin/framework/utils/i18n/LKI18NReader4ErrorCodes.java
@@ -28,28 +28,37 @@ public class LKI18NReader4ErrorCodes extends LKI18NReader {
 		if (code == null) {
 			return read(LKErrorCodesEnum.PARAM_ERROR, locale, params) + " [" + errorCode.toString() + "]";
 		}
-		if (code < 100000) {
-			return read(locale, "errorCodes", errorCode.toString(), params);
+		return read(locale, analysisFolderName(code), errorCode.toString(), params);
+	}
+
+
+	/**
+	 * 解析目录名
+	 * @param code 错误编码
+	 * @return 目录名
+	 */
+	private static String analysisFolderName(int code) {
+		if (code < 0) {// 负数为框架内部使用
+			return "errorCodes";
 		}
-		if (code < 110000) {
-			return read(locale, "app-core-errorCodes", errorCode.toString(), params);
+
+		int section = code / 10000;// 每一万一个段
+		switch (section) {
+			case 0:// 0-9999之间为框架内部使用
+				return "errorCodes";
+			case 1:// 10000-19999之间为具体项目使用编码
+				return "app-errorCodes";
+			default:// 其它段为projects框架使用
+				// projects-core -> 20000+ -> errorCodes-20000
+				// projects-activiti -> 30000+ -> errorCodes-30000
+				// projects-app -> 40000+ -> errorCodes-40000
+				// projects-attendance -> 50000+ -> errorCodes-50000
+				// projects-pss -> 60000+ -> errorCodes-60000
+				// projects-alipay -> 70000+ -> errorCodes-70000
+				// projects-wechatpay -> 80000+ -> errorCodes-80000
+				// projects-cashier-desk -> 90000+ -> errorCodes-90000
+				return "errorCodes-" + (section * 10000);
 		}
-		if (code < 120000) {
-			return read(locale, "app-app-errorCodes", errorCode.toString(), params);
-		}
-		if (code < 130000) {
-			return read(locale, "app-attendance-errorCodes", errorCode.toString(), params);
-		}
-		if (code < 140000) {
-			return read(locale, "app-pss-errorCodes", errorCode.toString(), params);
-		}
-		if (code < 150000) {
-			return read(locale, "app-activiti-errorCodes", errorCode.toString(), params);
-		}
-		if (code < 300000) {
-			return read(locale, "app-bus-errorCodes", errorCode.toString(), params);
-		}
-		return read(locale, "app-errorCodes", errorCode.toString(), params);
 	}
 
 
