240c05a3893ffec1cb0303c402d5a8ec4cc9e787
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-11-14 21:29:08 +0800

    错误编码整理

--- lichkin-framework-defines/src/main/java/com/lichkin/framework/defines/enums/impl/LKErrorCodesEnum.java
+++ lichkin-framework-defines/src/main/java/com/lichkin/framework/defines/enums/impl/LKErrorCodesEnum.java
@@ -40,26 +40,7 @@ public enum LKErrorCodesEnum implements LKCodeEnum {
 	/** 数据不存在 */
 	INEXIST(6),
 
-	/** 登录信息已失效 */
-	INVALIDED_TOKEN(7),
-
-	/** 公司信息有误 */
-	INVALIDED_COMP_ID(8),
-
-	/** 公司信息有误 */
-	INVALIDED_COMP_TOKEN(9),
-
-	/** 登录信息有误 */
-	INVALIDED_USER_LOGIN(10),
-
-	/** 账号不存在 */
-	ACCOUNT_INEXIST(11),
-
-	/** 公司不存在 */
-	COMP_INEXIST(12),
-
-	/** 部门不存在 */
-	DEPT_INEXIST(13);
+	;
 
 	/** 编码 */
 	private final Integer code;
