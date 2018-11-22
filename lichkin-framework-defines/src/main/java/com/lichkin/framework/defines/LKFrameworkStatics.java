97174a9bacfa7c112c56465e4e5b20f86dba9ec2
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-11-22 20:28:18 +0800

    取消分平台API路径

--- lichkin-framework-defines/src/main/java/com/lichkin/framework/defines/LKFrameworkStatics.java
+++ lichkin-framework-defines/src/main/java/com/lichkin/framework/defines/LKFrameworkStatics.java
@@ -12,26 +12,8 @@ public class LKFrameworkStatics {
 	/** 系统默认配置值：API数据请求映射（仅应用于客户端请求）。 */
 	public static final String WEB_MAPPING_API_APP = WEB_MAPPING_API + "/App";
 
-	/** 系统默认配置值：API数据请求映射（管理客户端）。 */
-	public static final String WEB_MAPPING_API_APP_ADMIN = WEB_MAPPING_API_APP + "/Admin";
-
-	/** 系统默认配置值：API数据请求映射（用户客户端）。 */
-	public static final String WEB_MAPPING_API_APP_USER = WEB_MAPPING_API_APP + "/User";
-
-	/** 系统默认配置值：API数据请求映射（员工客户端）。 */
-	public static final String WEB_MAPPING_API_APP_EMPLOYEE = WEB_MAPPING_API_APP + "/Employee";
-
 	/** 系统默认配置值：API数据请求映射（仅应用于页面请求）。 */
 	public static final String WEB_MAPPING_API_WEB = WEB_MAPPING_API + "/Web";
-
-	/** 系统默认配置值：API数据请求映射（管理平台）。 */
-	public static final String WEB_MAPPING_API_WEB_ADMIN = WEB_MAPPING_API_WEB + "/Admin";
-
-	/** 系统默认配置值：API数据请求映射（用户平台）。 */
-	public static final String WEB_MAPPING_API_WEB_USER = WEB_MAPPING_API_WEB + "/User";
-
-	/** 系统默认配置值：API数据请求映射（员工平台）。 */
-	public static final String WEB_MAPPING_API_WEB_EMPLOYEE = WEB_MAPPING_API_WEB + "/Employee";
 
 	/** 系统默认配置值：页面请求映射。 */
 	public static final String WEB_MAPPING_PAGES = ".dhtml";
