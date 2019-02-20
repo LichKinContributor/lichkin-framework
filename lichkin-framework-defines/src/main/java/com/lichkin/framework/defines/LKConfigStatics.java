675680215fdfe3c0eaa6c0cbff1e794601d712b7
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2019-02-20 16:41:39 +0800

    socket-server支持多服务端配置

--- lichkin-framework-defines/src/main/java/com/lichkin/framework/defines/LKConfigStatics.java
+++ lichkin-framework-defines/src/main/java/com/lichkin/framework/defines/LKConfigStatics.java
@@ -14,16 +14,16 @@ public class LKConfigStatics {
 	/** 已经实现的国际化类型列表，没有实现的国际化类型都将采用英文作为国际化类型。 */
 	public static Locale[] IMPLEMENTED_LOCALE_ARR = { Locale.SIMPLIFIED_CHINESE };
 
-	/** 系统默认配置值：系统标识。 */
+	/** 系统默认配置值：系统标识 */
 	public static String SYSTEM_TAG = "LichKin";
 
-	/** 系统默认配置值：系统名称。 */
+	/** 系统默认配置值：系统名称 */
 	public static String SYSTEM_NAME = "苏州鑫宏利业信息科技有限公司";
 
-	/** 系统默认配置值：是否启用调试功能。 */
+	/** 系统默认配置值：是否启用调试功能 */
 	public static boolean SYSTEM_DEBUG = false;
 
-	/** 系统默认配置值：是否启用页面调试功能。 */
+	/** 系统默认配置值：是否启用页面调试功能 */
 	public static boolean WEB_DEBUG = false;
 
 	/** 系统默认配置值：JS/CSS使用压缩 */
@@ -32,7 +32,10 @@ public class LKConfigStatics {
 	/** 系统默认配置值：环境上下文路径 */
 	public static String WEB_CONTEXT_PATH = "";
 
-	/** 系统默认配置值：是否启用页面调试功能。 */
+	/** 系统默认配置值：是否启用页面调试功能 */
 	public static boolean WEB_ADMIN_DEBUG = false;
+
+	/** 系统默认配置值：Scoket服务端配置文件索引值 */
+	public static int SOCKET_SERVER_CONFIG_IDX = 0;
 
 }
